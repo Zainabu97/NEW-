86c68f28852c4d5877018451abc5836dcc9aaa21
Zainabu97 <p2686969@my365.dmu.ac.uk>
2022-06-17 10:18:35 +0100

    I have added a few messages

--- reort2.c
+++ reort2.c
@@ -5,7 +5,7 @@ int main() {
 	printf("Hello world!");
 	printf("welcome!");
 	printf("please type your username and password");
-	printf("tell us about yourself");
+	printf("tell us about yourself including where you work");
 return 0;
 
 }
